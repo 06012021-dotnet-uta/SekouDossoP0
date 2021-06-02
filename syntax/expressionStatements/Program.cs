387aae9869d72420131d61bd8cf1c2b6b0f24b7b
SekouDosso <sekou.dosso@revature.net>
2021-06-02 07:44:00 -0400

    switch statement

---
+++ syntax/expressionStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace expressionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string s = "Revature"; 
+            // SwitchStatement(s);
+            SwitchStatement("Revature");
+        }
+
+        static void SwitchStatement(string arg){
+            int n = arg.Length;
+            switch (n){
+                case 0:
+                    Console.WriteLine("No argument");
+                    break;
+                case 1:
+                    Console.WriteLine("1 argument");
+                    break;
+                case 2:
+                    Console.WriteLine("2 arguments");
+                    break;
+                default:
+                    Console.WriteLine($"{n} arguments");
+                    break;
+            }
+        }
+    }
+}
