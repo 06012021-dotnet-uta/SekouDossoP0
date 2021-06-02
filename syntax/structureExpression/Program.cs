866f5fd1c1f15126275aaee173eaa364613449bf
SekouDosso <sekou.dosso@revature.net>
2021-06-02 07:02:50 -0400

    c# structure expression

--- syntax/structureExpression/Program.cs
+++ syntax/structureExpression/Program.cs
@@ -16,6 +16,16 @@ namespace structureExpression
             var a = 2 + 2 * 2 ;
             Console.WriteLine($"This a var a {a}");
             // output => This a var a 6
+
+            int c  = 13 / 5/ 3 ;
+            int b = 13/(5/2);
+            Console.WriteLine($"This var a: {a} this int b: {b} this int a: {c}");
+            // output => This var a: 6 this int b: 6 this int a: 0
+
+            a = (2 + 2) * 2 ;
+            Console.WriteLine($"This a var a {a}");
+
+
         }
     }
 }
