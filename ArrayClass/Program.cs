de5bb67eb54d1d65ccd1c56e806dceccc0fe69ab
SekouDosso <sekou.dosso@revature.net>
2021-06-03 08:53:39 -0400

    Array class

--- ArrayClass/Program.cs
+++ ArrayClass/Program.cs
@@ -8,7 +8,6 @@ namespace ArrayClass
         {
             // declare a single dimensional array  
             int[] a1 = new int[5]; 
-
             // declare and set element value 
             int[] a2 = new int[5] {1, 2, 3, 4, 5}; 
 
@@ -17,13 +16,11 @@ namespace ArrayClass
 
             // declare a two dimensional array  
             int[,] multiDimensionalArray1 = new int[2, 3]; 
-
              // declare and set element value in 2 dimensional array
             int[,] multiDimensionalArray2 = { {1, 2, 3}, {4, 5,6} }; 
-
+            
             // declare a jagged array 
             int[][] jaggedArray = new int[6][];
-
             // set the values of the  first array in the jagged array structure 
             jaggedArray[0] = new int[4] {1, 2, 3, 4};
             
