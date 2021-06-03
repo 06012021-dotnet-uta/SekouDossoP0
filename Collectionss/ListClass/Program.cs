4c83e8bc3927bd21d5be017a8330e9e7338bafed
SekouDosso <sekou.dosso@revature.net>
2021-06-03 10:20:23 -0400

    List class

--- Collectionss/ListClass/Program.cs
+++ Collectionss/ListClass/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ListClass
 {
@@ -6,7 +8,22 @@ namespace ListClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create a list of string by using a collection initializer. 
+            var salmons = new List<string> { "chinook", "coho", "pink"};
+            // public List<string> salmons = new List<string>();
+            // salmons.Add("chinook");
+            // salmons.Add("coho");
+            // salmons.Add("pink");
+            salmons.Add("sockey");
+
+            // remove an element from the list by specifying the object 
+            salmons.Remove("coho");
+
+            // iterate through the list
+            foreach ( var salmon in salmons ){
+                Console.WriteLine(salmon + " ");
+            }
+
         }
     }
 }
