4ce0d418a38cf9c8c0f2f8bf813d5e43fee1ebf4
SekouDosso <sekou.dosso@revature.net>
2021-06-04 04:37:17 -0400

    clean up

--- RockPaperScissors1/Program.cs
+++ RockPaperScissors1/Program.cs
@@ -12,7 +12,7 @@ namespace RockPaperScissors1
             PlayerDerivedClass player1 = new PlayerDerivedClass();
             PlayerDerivedClass computer = new PlayerDerivedClass("Max", "HeadRoom", 38);
             Console.WriteLine(rpsGame.WelcomeMessage());
-            bool successfulConversion = false;
+            // bool successfulConversion = false;
             int playerChoiceInt;
             string quitter = "n";
 
@@ -28,17 +28,11 @@ namespace RockPaperScissors1
                 int tieRounds = 0;
                 while (computerRoundWins < 2 && playerRoundWins < 2)
                 {
-                    //a do/while loop runs at least once, while a while loop may not ever run.
-                    //gets the players choice
+
                     // get  player choice 
                         playerChoiceInt = PlayerChoice.getPlayerChoice();
                     
-                    //you can omit the {} if the body of hte statement is only 1 line or even put it all on one line.
-                    // if (successfulConversion == true) Console.WriteLine($"the conversion returned {successfulConversion} and the player chose {playerChoiceInt}");
-                    // else
-                    //     Console.WriteLine($"the conversion returned {successfulConversion} and the player chose {playerChoiceInt}");
 
-                    //get a random number generator object from the Random Class
                     Random rand = new Random();
                     //get a random number 1,2, or 3.
                     int computerChoice = rand.Next(1, Enum.GetNames(typeof(RpsChoice)).Length + 1);
@@ -67,9 +61,6 @@ namespace RockPaperScissors1
                         playerRoundWins++;
                     }
 
-                    //you can get typeDef the number to the equivalent RpsChoice Enum.
-                    // Console.WriteLine((RpsChoice)playerChoiceInt);
-                    // Console.WriteLine((RpsChoice)computerChoice);
 
                 }//end of rounds
 
