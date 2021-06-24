892a03fc38ea2c2ab3358630995f3cfbe260cece
SekouDosso <sekou.dosso@revature.net>
2021-06-24 06:26:24 -0400

    Hacker Rank week1 Array sum

---
+++ HackerRank/ArraySum.cs
@@ -0,0 +1,33 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class ArraySum
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+         int s = 0;
+        for(int i = 0 ; i<ar.Count; i++){
+            s += ar[i];            
+        }
+        return s;
+    }
+
+}
