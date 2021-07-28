b10b5337f4f7a4129d7e4c5a771b15360e781c2c
SekouDosso <sekou.dosso@revature.net>
2021-07-28 08:32:04 -0400

    coding challenge

--- HackerRank/week3/sqlStation9.sql
+++ HackerRank/week3/sqlStation9.sql
@@ -1,4 +1,26 @@
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) 
+-- from STATION. Your result cannot contain duplicates.
+select distinct city 
+from STATION 
+where city like '[aeiuo]%';
+
+-- Query the list of CITY names from STATION that do 
+-- not start with vowels. Your result cannot contain duplicates.
 select distinct City
 from STATION
--- where City not like '[aeiou]%';
+-- where City not like '[^aeiou]%';
 where City like '[QWRTPSDFGHJKLZXCVBNMY]%' ;
+
+-- Query the list of CITY names from STATION that do not 
+-- end with vowels. Your result cannot contain duplicates.
+select distinct city 
+from STATION
+where city like '%[^aeiuo]';
+
+-- Query the list of CITY names from STATION that do not start 
+-- with vowels and do not end with vowels. Your result cannot 
+-- contain duplicates.
+select distinct city 
+from STATION
+where city like '[^aeuio]%' and city like '%[^aeuoi]';
+
