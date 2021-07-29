df64a1f1a3ae33c3bc7addd6f91be61c70e3966e
SekouDosso <sekou.dosso@revature.net>
2021-07-29 08:31:53 -0400

    Hacker rank Station17

--- HackerRank/week3/NewCompany.sql
+++ HackerRank/week3/NewCompany.sql
@@ -7,3 +7,36 @@ select company_code, founder,
 from Company 
 order by company_code;
 
+select Company.company_code, Company.founder,
+    count(distinct Lead_Manager.lead_manager_code),
+    count(distinct Senior_Manager.senior_manager_code),
+    count(distinct Manager.manager_code),
+    count(distinct Employee.employee_code)
+from Company
+join Lead_Manager
+    on Lead_Manager.company_code = Company.company_code
+join Senior_Manager
+    on Senior_Manager.company_code = Company.company_code
+join Manager
+    on Manager.company_code = Company.company_code
+join Employee
+    on Employee.company_code = Company.company_code
+
+group by Company.company_code , Company.founder
+order by Company.company_code asc;
+-- ***************
+select Company.company_code, Company.founder,
+    count(distinct Lead_Manager.lead_manager_code),
+    count(distinct Senior_Manager.senior_manager_code),
+    count(distinct Manager.manager_code),
+    count(distinct Employee.employee_code)
+   
+from Company, Lead_Manager, Senior_Manager, Manager, Employee
+
+where Lead_Manager.company_code = Company.company_code
+    and Senior_Manager.company_code = Company.company_code
+    and Manager.company_code = Company.company_code
+    and Employee.company_code = Company.company_code
+
+group by Company.company_code , Company.founder
+order by Company.company_code asc;
